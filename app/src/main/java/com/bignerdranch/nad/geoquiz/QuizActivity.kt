c71f6d6a9985c5d0d731fdd4902a38c69aa5f0b2
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2018-09-18 16:09:20 +0700

    add log activity cycle

--- app/src/main/java/com/bignerdranch/nad/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/bignerdranch/nad/geoquiz/QuizActivity.kt
@@ -1,7 +1,9 @@
 package com.bignerdranch.nad.geoquiz
 
+import android.nfc.Tag
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
@@ -12,10 +14,13 @@ class QuizActivity : AppCompatActivity() {
 //    private var mTrueButton: Button? = null
     private lateinit var mTrueButton: Button
     private lateinit var mFalseButton: Button
+    private var TAG: String? = "QuizActivity"
+//    private static final String TAG = "QuizActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+        Log.i(TAG, "oncreate")
 
         mTrueButton = findViewById(R.id.true_button) as Button;
         mTrueButton.setOnClickListener {
@@ -28,5 +33,30 @@ class QuizActivity : AppCompatActivity() {
         mFalseButton.setOnClickListener {
             Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_LONG).show()
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
+    }
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(TAG, "onRestart")
     }
 }
