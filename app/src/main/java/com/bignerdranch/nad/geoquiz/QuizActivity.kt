d724cca1e7de31b4e7399e0e88262d345af30eb5
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2018-09-18 15:54:30 +0700

    move toast to top

--- app/src/main/java/com/bignerdranch/nad/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/bignerdranch/nad/geoquiz/QuizActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.nad.geoquiz
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Gravity
 import android.widget.Button
 import android.widget.Toast
 
@@ -19,7 +20,9 @@ class QuizActivity : AppCompatActivity() {
         mTrueButton = findViewById(R.id.true_button) as Button;
         mTrueButton.setOnClickListener {
             // make a toast on button click event
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG).show()
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_LONG)
+            toast.setGravity(Gravity.TOP, 0, 0)
+            toast.show()
         }
         mFalseButton = findViewById(R.id.false_button) as Button;
         mFalseButton.setOnClickListener {
